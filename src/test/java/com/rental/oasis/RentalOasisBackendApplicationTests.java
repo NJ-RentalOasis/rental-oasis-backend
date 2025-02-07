b0245b1179e49bc86840016db19a4c6264435c33
Njabs <admin@rentaloasis.co.za>
2025-02-07 15:42:00 +0200

    Initial commit

---
+++ src/test/java/com/rental/oasis/RentalOasisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.oasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalOasisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
